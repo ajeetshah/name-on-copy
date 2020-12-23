4895fd7bc453587777e5d0d274446b654d38b200
Ajeet Shah <ajeets.shah@hotmail.com>
2020-12-23 23:29:28 +0530

    Add dist

--- src/index.js
+++ src/index.js
@@ -1,40 +1,41 @@
-;(function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define('nameOnCopy', [], factory)
-  } else if (typeof module === 'object' && module.exports) {
-    module.exports = factory()
-  } else {
-    root.nameOnCopy = factory()
-  }
-})(typeof self !== 'undefined' ? self : this, function () {
-  function isNumber(value) {
-    return !isNaN(+value)
-  }
+// ;(function (root, factory) {
+//   if (typeof define === 'function' && define.amd) {
+//     define('nameOnCopy', [], factory)
+//   } else if (typeof module === 'object' && module.exports) {
+//     module.exports = factory()
+//   } else {
+//     root.nameOnCopy = factory()
+//   }
+// })(typeof self !== 'undefined' ? self : this, function () {
 
-  function isUnique(name, names) {
-    return names.indexOf(name) === -1
-  }
+function isNumber(value) {
+  return !isNaN(+value)
+}
 
-  function increment(name) {
-    const lastChar = name.slice(-1)
-    if (isNumber(lastChar)) {
-      const nameWithoutLastIndex = name.slice(0, name.length - 1)
-      return nameWithoutLastIndex + (+lastChar + 1)
-    }
-    return name + 2
-  }
+function isUnique(name, names) {
+  return names.indexOf(name) === -1
+}
 
-  function generate(
-    source = '',
-    names = [],
-    options = { suffix: 'copy', useSpaces: false }
-  ) {
-    let name = source + (options.useSpaces ? ' ' : '') + options.suffix
-    while (!isUnique(name, names)) {
-      name = increment(name)
-    }
-    return name
+function increment(name) {
+  const lastChar = name.slice(-1)
+  if (isNumber(lastChar)) {
+    const nameWithoutLastIndex = name.slice(0, name.length - 1)
+    return nameWithoutLastIndex + (+lastChar + 1)
   }
+  return name + 2
+}
 
-  return generate
-})
+function generate(
+  source = '',
+  names = [],
+  options = { suffix: 'copy', useSpaces: false }
+) {
+  let name = source + (options.useSpaces ? ' ' : '') + options.suffix
+  while (!isUnique(name, names)) {
+    name = increment(name)
+  }
+  return name
+}
+
+//   return generate
+// })
