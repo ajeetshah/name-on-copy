3fb3ff6205702618eefb840f7adc88db7c959ef0
Ajeet Shah <ajeets.shah@hotmail.com>
2020-12-26 01:36:21 +0530

    name-on-copy.js

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, ''),
-    filename: 'index.js',
+    filename: 'name-on-copy.js',
     library: 'nameOnCopy',
     libraryTarget: 'umd',
     globalObject: 'this',
