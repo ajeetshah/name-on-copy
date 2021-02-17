13b0d56ee80e9cf757ed6a524200688bbb08b16e
Ajeet Shah <ajeets.shah@hotmail.com>
2021-02-17 23:51:09 +0530

    Run build

---
+++ utils.d.ts
@@ -0,0 +1,9 @@
+export declare function isNumber(value: string): boolean;
+export declare function isUnique(value: string, values: string[]): boolean;
+export declare function getFileNameAndExtension(value: string): {
+    fileName: string;
+    extension: string;
+};
+export declare function increment(value: string): string;
+export declare function generateFirst(source: string, suffix: string, useSpaces: boolean, isFileName: boolean): string;
+export declare function generateNext(name: string, isFileName: boolean): string;
